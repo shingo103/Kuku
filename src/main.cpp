867535e4464d5e808726cf65b1a9cd607cbe3730
Shingo Tomioka <shingo@tomiokashingo-no-MacBook-Pro.local>
2015-12-21 19:10:05 +0900

    CHG: long option

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include<unistd.h>
+//#include<unistd.h>
+#include<getopt.h>
 #include<iostream>
 #include<fstream>
 #include<string>
@@ -6,6 +7,7 @@
 using namespace std;
 
 const string VERSION = "1.0.0";
+
 
 void print_table(int x, int y, bool add){
     for(int i = 1; i <= x; i++){
@@ -17,36 +19,49 @@ void print_table(int x, int y, bool add){
 }
 
 
-void print_help(){
-    
+void show_help(){
+    cerr << "Usage: kuku [-h|--help] [-v|--version] [-a|--add] x y" << endl;
+    cerr << "Positional arguments:" << endl;
+    cerr << "\tx\tthe number of rows (integer)" << endl;
+    cerr << "\ty\tthe number of columns (integer)" << endl;
+    cerr << "Optional arguments:" << endl;
+    cerr << "\t-h --help\tshow this message and exit" << endl;
+    cerr << "\t-v --version\tshow program version and exit" << endl;
+    cerr << "\t-a --add\tadd two integers, not multiply" << endl;
 }
 
+
 int main(int argc, char *argv[]){
-    bool add = false;
+    struct option longopts[] = {
+        {"help", no_argument, NULL, 'h'},
+        {"version", no_argument, NULL, 'v'},
+        {"add", no_argument, NULL, 'a'}
+    };
+    bool add_flag = false;
     int opt;
-    while((opt = getopt(argc, argv, "hvax:y:")) != -1){
+    while((opt = getopt_long(argc, argv, "hva", longopts, NULL)) != -1){
         switch(opt){
         case 'h':
-            print_help();
+            show_help();
             return 1;
             break;
-        case 'v': {
+        case 'v':
             cout << "kuku version "<< VERSION << endl;
             return 1;
             break;
-            }
         case 'a':
-            add=true;
+            add_flag=true;
             break;
         }
     }
     if(argc - optind != 2){
-        cout << "Give two integers." << endl;
+        cerr << "Error: Give two integers." << endl;
+        show_help();
     }
     else{
         int x = atoi(argv[optind]);
         int y = atoi(argv[optind + 1]);
-        print_table(x, y, add);
+        print_table(x, y, add_flag);
     }
     return 0;
 }
