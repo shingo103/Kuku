3f760cf851b4b4d8ff916c4c0f65e84640a4a3b4
Shingo Tomioka <shingo@tomiokashingo-no-MacBook-Pro.local>
2015-12-23 15:45:58 +0900

    NEW: Added system test in .travis.yml

--- test/system_test.sh
+++ test/system_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-../waf build
-
 mytest()
 {
     echo "try   : $1"
@@ -10,21 +8,35 @@ mytest()
     echo -n "result: "
     if [ "$output" = "$target" ] ; then
 	echo passed
+	return 0
     else
 	echo failed
+	return 1
     fi
     echo
 }
 
 target=`echo -e " 1  2  3  4 \n 2  4  6  8 \n 3  6  9 12 "`
 mytest "../build/kuku 3 4" "$target"
+result=$?
 
 target=`echo -e " 2  3  4  5 \n 3  4  5  6 \n 4  5  6  7 "`
 mytest "../build/kuku 3 4 -a" "$target"
+result=`[ $? -eq 0 ]  && [ $result -eq 0 ] ; echo $?`
 
 target="Error: Arguments must be integers. '3.1' was given."
 mytest "../build/kuku 3.1 4" "$target"
+result=`[ $? -eq 0 ]  && [ $result -eq 0 ] ; echo $?`
 
 target="Error: Arguments must be integers. 'abc' was given."
 mytest "../build/kuku abc 4" "$target"
+result=`[ $? -eq 0 ]  && [ $result -eq 0 ] ; echo $?`
 
+echo -ne "\nwhole : "
+if [ $result -eq 0 ] ; then
+    echo "passed"
+    exit 0
+else
+    echo "failed"
+    exit 1
+fi
